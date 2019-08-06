d551bb6299a46edbffc918f022effda4c3e8cb52
subsevy <sub0713@gmail.com>
2019-08-06 17:16:42 +0900

    Editing a Video

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -41,6 +41,7 @@ export const videoDetail = async (req, res) => {
   } = req;
   try {
     const video = await Video.findById(id);
+    console.log(video.id);
     res.render("videoDetail", { pageTitle: "Video Detail", video });
   } catch (error) {
     console.log(error);
@@ -48,8 +49,30 @@ export const videoDetail = async (req, res) => {
   }
 };
 
-export const editVideo = (req, res) =>
-  res.render("editVideo", { pageTitle: "Edit Video" });
+export const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("editVideo", { pageTitle: `Edit ${video.title}`, video });
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
+
+export const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description }
+  } = req;
+  try {
+    await Video.findOneAndUpdate({ id }, { title, description });
+    res.redirect(routes.videoDetail(id));
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 
 export const deleteVideo = (req, res) =>
   res.render("deleteVideo", { pageTitle: "Delete Video" });
