cd49e586c748c513d4fe856452cd4f293358620d
subsevy <sub0713@gmail.com>
2019-08-08 10:15:13 +0900

    Github Login 3

--- routes.js
+++ routes.js
@@ -35,9 +35,8 @@ const routes = {
   userDetail: id => {
     if (id) {
       return `/users/${id}`;
-    } else {
-      return USER_DETAIL;
     }
+    return USER_DETAIL;
   },
   editProfile: EDIT_PROFILE,
   changePassword: CHANGE_PASSWORD,
@@ -46,23 +45,20 @@ const routes = {
   videoDetail: id => {
     if (id) {
       return `/videos/${id}`;
-    } else {
-      return VIDEO_DETAIL;
     }
+    return VIDEO_DETAIL;
   },
   editVideo: id => {
     if (id) {
       return `/videos/${id}/edit`;
-    } else {
-      return EDIT_VIDEO;
     }
+    return EDIT_VIDEO;
   },
   deleteVideo: id => {
     if (id) {
       return `/videos/${id}/delete`;
-    } else {
-      return DELETE_VIDEO;
     }
+    return DELETE_VIDEO;
   },
   github: GITHUB,
   githubCallback: GITHUB_CALLBACK
