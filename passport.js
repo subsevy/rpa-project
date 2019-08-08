ecb5e10fcdb3eb25d1f878c59136ea79d5fa70c6
subsevy <sub0713@gmail.com>
2019-08-08 13:30:57 +0900

    Facebook Login 2

--- passport.js
+++ passport.js
@@ -26,7 +26,9 @@ passport.use(
     {
       clientID: process.env.FB_ID,
       clientSecret: process.env.FB_SECRET,
-      callbackURL: `http://localhost:4000${routes.facebookCallback}`
+      callbackURL: `https://soft-fly-32.localtunnel.me${routes.facebookCallback}`,
+      profileFields: ["id", "displayName", "photos", "email"],
+      scope: ["public_profile", "email"]
     },
     facebookLoginCallback
   )
